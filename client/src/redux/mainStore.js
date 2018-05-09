74e63e49be3b137d8d33ef1b435f6776ad65f521
Skanarky <kutkurov@gmail.com>
2018-05-09 17:10:38 -0600

    cleared old code

--- client/src/redux/mainStore.js
+++ client/src/redux/mainStore.js
@@ -6,6 +6,10 @@ import youtubeReducer from "./youtube";
 import udemyReducer from "./udemy";
 import courseraReducer from "./coursera";
 
-const store = createStore(combineReducers({videos: youtubeReducer, courses: udemyReducer, coCourses: courseraReducer}), applyMiddleware(thunk));
+const store = createStore(combineReducers({
+    videos: youtubeReducer,
+    courses: udemyReducer,
+    coCourses: courseraReducer
+}), applyMiddleware(thunk));
 
 export default store;
