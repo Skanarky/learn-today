c0be94fcd00ec71e141b3d4fe902126f9b09f420
Skanarky <kutkurov@gmail.com>
2018-06-21 15:43:18 -0600

    fixed link for YouTube API search

--- routes/videos.js
+++ routes/videos.js
@@ -4,14 +4,14 @@ const videoRouter = express.Router();
 
 videoRouter.route("/")
     .get((req, res) => {
-        axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=learn+photography&maxResults=30&type=video&key=${process.env.YOUTUBE_PASS}`)
+        axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=learn&maxResults=30&type=video&key=${process.env.YOUTUBE_PASS}`)
         .then(response => res.status(200).send(response.data.items))
             .catch(() => res.status(404).send());
     });
 
 videoRouter.route("/:searchKey")
     .get((req, res) => {
-        axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=learn+photography+${req.params.searchKey}&maxResults=40&type=video&key=${process.env.YOUTUBE_PASS}`)
+        axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=learn+${req.params.searchKey}&maxResults=40&type=video&key=${process.env.YOUTUBE_PASS}`)
         .then(response => res.status(200).send(response.data.items))
             .catch(() => res.status(404).send());
     });
